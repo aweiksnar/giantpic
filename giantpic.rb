9f64c0d672d5e239fee8768afcc9f1c1b17a5585
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-08-28 17:15:30 -0500

    datamapper working

--- giantpic.rb
+++ giantpic.rb
@@ -1,6 +1,39 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'data_mapper'
+require 'dm-sqlite-adapter'
 
-get '/' do
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/giantpic.db")
+
+class Picture
+  include DataMapper::Resource
+
+  property :id,       Serial
+  property :url,      String
+  property :title,    String
+  property :caption,  Text
+end
+
+get "/" do
+  redirect :index
+end
+
+get "/index" do
+  @pictures = Picture.all
   erb :index
 end
+
+post "/index" do
+  @picture = Picture.create(params[:picture])
+  "Saved" unless @picture.nil?
+end
+
+get "/new" do
+  erb :new
+end
+
+
+DataMapper.finalize
+
+Picture.auto_upgrade!
+
