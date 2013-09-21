62b81a6cb5a6c1c3c870b73a7d1b9210658f74e8
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-09-20 19:42:22 -0500

    length on email address

--- giantpic.rb
+++ giantpic.rb
@@ -26,7 +26,7 @@ class User
   include DataMapper::Resource
 
   property :id,       Serial
-  property :email,    String, :format => :email_address, :required => true, :unique => true
+  property :email,    String, :format => :email_address, :required => true, :unique => true, :length => 255
   property :password, BCryptHash, :required => true, :length => 60
 
   has n, :pictures
